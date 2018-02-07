19a4205a4b950dbc0629ad0129aa3c64b15cfd0a
Matthias Gernand <m.gernand@fluxera.com>
2018-02-07 15:55:15 +0100

    Created first versions of k8s deployment configuration scripots within helm chart.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kubectl create ns openbrisk
+kubectl create ns openbrisk-functions
 helm init
 #helm package charts/openbrisk
 helm install --name openbrisk --namespace openbrisk charts/openbrisk
